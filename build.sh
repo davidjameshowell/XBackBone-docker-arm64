92dac60aee599e28ecdf5e753a58a9f3c16d7092
David Howell <david@davidjameshowell.com>
2023-07-18 22:12:27 +0000

    Update for PHP 8.22 and XBackBOne 3.6.3

--- build.sh
+++ build.sh
@@ -11,12 +11,12 @@ cd "${SCRIPT_DIR}"/upstream_images/toolbox || exit
 docker build -t nowaidavid/xbackbone-toolbox:latest -t nowaidavid/xbackbone-toolbox:"${GIT_SHA_VAR:-nosha}" .
 
 # Build PHP image
-cd "${SCRIPT_DIR}"/upstream_images/php-official_7.4-alpine-arm64 || exit
-docker build -t nowaidavid/xbackbone-php:7.4-alpine-arm64 -t nowaidavid/xbackbone-php:7.4-alpine-arm64-"${GIT_SHA_VAR:-nosha}" .
+cd "${SCRIPT_DIR}"/upstream_images/php-official_8.2-alpine-arm64 || exit
+docker build -t nowaidavid/xbackbone-php:8.2-alpine-arm64 -t nowaidavid/xbackbone-php:8.22-alpine-arm64-"${GIT_SHA_VAR:-nosha}" .
 
 # Build PHP-NGINX iamge
-cd "${SCRIPT_DIR}"/upstream_images/php-nginx_alpine-php7-arm64 || exit
-docker build -t nowaidavid/xbackbone-php-nginx:alpine-php7-arm64 -t nowaidavid/xbackbone-php-nginx:alpine-php7-arm64-"${GIT_SHA_VAR:-nosha}" .
+cd "${SCRIPT_DIR}"/upstream_images/php-nginx_alpine-php8-arm64 || exit
+docker build -t nowaidavid/xbackbone-php-nginx:alpine-php8-arm64 -t nowaidavid/xbackbone-php-nginx:alpine-php8-arm64-"${GIT_SHA_VAR:-nosha}" .
 
 # Build XBackBone image
 cd "${SCRIPT_DIR}"/src || exit
@@ -26,11 +26,11 @@ docker build -t nowaidavid/xbackbone-docker-arm64:latest -t nowaidavid/xbackbone
 docker image push nowaidavid/xbackbone-toolbox:latest
 docker image push nowaidavid/xbackbone-toolbox:"${GIT_SHA_VAR:-nosha}"
 
-docker image push nowaidavid/xbackbone-php:7.4-alpine-arm64
-docker image push nowaidavid/xbackbone-php:7.4-alpine-arm64-"${GIT_SHA_VAR:-nosha}"
+docker image push nowaidavid/xbackbone-php:8.2-alpine-arm64
+docker image push nowaidavid/xbackbone-php:8.2-alpine-arm64-"${GIT_SHA_VAR:-nosha}"
 
-docker image push nowaidavid/xbackbone-php-nginx:alpine-php7-arm64
-docker image push nowaidavid/xbackbone-php-nginx:alpine-php7-arm64-"${GIT_SHA_VAR:-nosha}"
+docker image push nowaidavid/xbackbone-php-nginx:alpine-php8-arm64
+docker image push nowaidavid/xbackbone-php-nginx:alpine-php8-arm64-"${GIT_SHA_VAR:-nosha}"
 
 docker image push nowaidavid/xbackbone-docker-arm64:latest
 docker image push nowaidavid/xbackbone-docker-arm64:"${GIT_SHA_VAR:-nosha}"
